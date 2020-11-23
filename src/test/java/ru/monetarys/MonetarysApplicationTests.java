c7d65595a5286dbd32376862fd25eeb47df6bf92
Artem <marzpg80@gmail.com>
2020-11-23 13:43:19 +0300

    added tests

--- src/test/java/ru/monetarys/MonetarysApplicationTests.java
+++ src/test/java/ru/monetarys/MonetarysApplicationTests.java
@@ -1,13 +1,39 @@
 package ru.monetarys;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class MonetarysApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testIsUnauthorized() throws Exception {
+		mockMvc.perform(get("http://localhost:8080/"))
+				.andDo(print())
+				.andExpect(status().isUnauthorized());
+	}
+
+	@Test
+	void testIsAuthorized() throws Exception {
+		mockMvc.perform(get("http://localhost:8080/").with(httpBasic("Ulxanxv", "1234")))
+				.andDo(print())
+				.andExpect(status().is2xxSuccessful());
 	}
 
 }
