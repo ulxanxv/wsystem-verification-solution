2ef0375905d2228f5f1997df87e239bd69733083
Artem <marzpg80@gmail.com>
2020-12-02 15:47:31 +0300

    added annotations to fields

--- src/main/java/ru/monetarys/models/Transfer.java
+++ src/main/java/ru/monetarys/models/Transfer.java
@@ -18,70 +18,91 @@ import java.time.LocalDateTime;
 public class Transfer {
 
     @Id
+    @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Length(max = 32)
+    @Column
     @NotNull
+    @Length(max = 32)
     private String operationCode;
 
+    @Column
     @NotNull
     private BigDecimal amount;
 
-    @Length(max = 3)
+    @Column
     @NotNull
+    @Length(max = 3)
     private String currency;
 
+    @Column
     @Length(max = 36)
     private String transactionId;
 
+    @Column
     @Length(max = 1024)
     private String description;
 
+    @Column
     @Length(max = 1024)
     private String messageToPayee;
 
-    @Enumerated(EnumType.STRING)
+    @Column
     @NotNull
+    @Enumerated(EnumType.STRING)
     private TransferStatus status;
 
+    @Column
     @Length(max = 22)
     private String payerAccount;
 
+    @Column
     @Length(max = 128)
     private String payerLastName;
 
+    @Column
     @Length(max = 128)
     private String payerFirstName;
 
+    @Column
     @Length(max = 128)
     private String payerSubName;
 
+    @Column
     @Length(max = 11)
     private String payerPhone;
 
+    @Column
     @Length(max = 160)
     private String payeeBank;
 
+    @Column
     @Length(max = 22)
     private String payeeAccount;
 
+    @Column
     @Length(max = 128)
     private String payeeLastName;
 
+    @Column
     @Length(max = 128)
     private String payeeFirstName;
 
+    @Column
     @Length(max = 128)
     private String payeeSubName;
 
+    @Column
     @Length(max = 11)
     private String payeePhone;
 
-    @CreatedDate
+    @Column
     @NotNull
+    @CreatedDate
     private LocalDateTime creationDate;
 
+    @Column
     @LastModifiedDate
     private LocalDateTime lastUpdateDate;
 
