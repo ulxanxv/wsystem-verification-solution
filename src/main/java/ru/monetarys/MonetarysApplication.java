d60b59f108d660a43cfda6563bdeda3ac107ea51
Artem <marzpg80@gmail.com>
2020-12-03 13:47:59 +0300

    deleted unnecessary rows

--- src/main/java/ru/monetarys/MonetarysApplication.java
+++ src/main/java/ru/monetarys/MonetarysApplication.java
@@ -2,18 +2,14 @@ package ru.monetarys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import ru.monetarys.messages.TransferSender;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class MonetarysApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext run = SpringApplication.run(MonetarysApplication.class, args);
-		TransferSender transferSender = run.getBean("transferSender", TransferSender.class);
-		transferSender.sendMessage();
+		SpringApplication.run(MonetarysApplication.class, args);
 	}
 
 }
