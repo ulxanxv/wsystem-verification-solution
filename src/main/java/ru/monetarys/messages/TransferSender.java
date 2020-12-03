1ddd7ab7e0dd196c359a502dfc28621416ffb49e
Artem <marzpg80@gmail.com>
2020-12-03 19:00:01 +0300

    added message sending

--- src/main/java/ru/monetarys/messages/TransferSender.java
+++ src/main/java/ru/monetarys/messages/TransferSender.java
@@ -5,6 +5,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
+import ru.monetarys.config.RabbitMqConfiguration;
+import ru.monetarys.messages.entities.TransferRequest;
+
+import java.util.UUID;
 
 @Data
 @Slf4j
@@ -14,8 +18,9 @@ public class TransferSender {
 
     private final RabbitTemplate rabbitTemplate;
 
-    public void sendMessage() {
-        // TODO: sending message...
+    public void sendMessage(TransferRequest request) {
+        request.getHeader().setMessageId(UUID.randomUUID().toString());
+        rabbitTemplate.convertAndSend(RabbitMqConfiguration.IN_EXCHANGE_NAME, RabbitMqConfiguration.ROUTING_KEY, request);
     }
 
 }
