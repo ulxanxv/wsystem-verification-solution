11b9bd424f0c6de9571a50f1f7a8c763e0d73525
Artem <marzpg80@gmail.com>
2020-12-11 23:26:25 +0300

    small fix

--- src/main/java/ru/monetarys/messages/TransferConsumer.java
+++ src/main/java/ru/monetarys/messages/TransferConsumer.java
@@ -1,6 +1,7 @@
 package ru.monetarys.messages;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
@@ -9,7 +10,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import ru.monetarys.messages.entities.TransferRequest;
 
-@Data
+@Getter
 @Slf4j
 @Service
 @RequiredArgsConstructor
